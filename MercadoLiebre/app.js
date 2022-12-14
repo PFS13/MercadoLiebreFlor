02750fe2c92328bb6f79398f9e22ad31d18011c5
PFS13 <floor.saan@live.com.ar>
2022-12-14 18:46:17 -0300

    Agregué el trabajo al repositorio

---
+++ MercadoLiebre/app.js
@@ -0,0 +1,13 @@
+const express = require ("express");
+const path = require("path");
+const app = express ();
+
+const port =3000;
+
+app.listen (port, ( )=> console.log ("Servidor escuchando puerto")); // para levanar    
+
+app.use (express.static("public")); 
+app.get ("/", (req,res) =>  res.sendFile(path.join(__dirname , "/views/home.html")));
+app.get ("/register", (req,res) =>  res.sendFile(path.join(__dirname , "/views/register.html"))); // unifica el app.js con mi html que se encuentra en la carpeta VIEWS
+app.get ("/login", (req,res) =>  res.sendFile(path.join(__dirname , "/views/login.html")));
+// Hay que agregar uun.gitignore en la raiz de mi proyecto y dentro escribir node_modules.
